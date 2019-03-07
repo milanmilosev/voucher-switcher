939ff1073a782c1cb8ed6a341465e929ec4416a9
milanmilosev <milan.milosev@deliveryhero.com>
2019-03-07 15:36:50 +0100

    CORS updated

--- app.js
+++ app.js
@@ -10,11 +10,16 @@ const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
 
 // Allow CORS
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
     next();
 });
+
 
 const myVars = {
     domain: 'dev-w6903s3t.eu.auth0.com',
